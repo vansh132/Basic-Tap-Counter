667f92e2d93a08bb62191a6d6b7aef1abd993ab7
vansh132 <shah.vansh132@gmail.com>
2022-09-07 19:10:29 +0530

    Final Commit

---
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.introtocompose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.introtocompose.ui.theme.IntroToComposeTheme
+import com.example.introtocompose.ui.theme.Shapes
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IntroToComposeTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun MyApp() {
+    val count = remember {
+        mutableStateOf(0)
+    }
+    Surface(
+         modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF546E7A)
+    ) {
+        Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "$${count.value}",
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 39.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            Spacer(modifier = Modifier.height(100.dp))
+            createCircle(count.value) {
+                newValue -> count.value = newValue
+            }
+        }
+    }
+}
+
+@Composable
+fun createCircle(moneyCounter: Int = 0, updatedMoneyCounter: (Int) -> Unit) {
+
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(105.dp)
+        .clickable {
+                   updatedMoneyCounter(moneyCounter+100)
+        }
+        ,
+        elevation = 10.dp
+        ,
+        shape = CircleShape) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap", modifier = Modifier)
+        }
+    }
+}
